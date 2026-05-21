69c52a1ccdb8cdfd24fdbc89d130f37500104a10
Josef31 <josef310798@gmail.com>
2026-05-21 16:17:23 -0400

    ejercicios 9 y 11

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import {ej01} from './ej01.js';
 import {ej03} from './ej03.js';
 import {ej05} from './ej05.js';
 import {ej07} from './ej07.js';
+import {ej09} from './ej09.js';
+import {ej11} from './ej11.js';
 
 console.log("Ejercicio 1: ");
 ej01();
@@ -19,6 +21,12 @@ console.log("Ejercicio 7: ");
 ej07();
 console.log('============================');
 
+console.log("Ejercicio 9: ");
+ej09();
+console.log('============================');
 
+console.log("Ejercicio 11: ");
+ej11();
+console.log('============================');
 
 console.log("hola mundo");
