a9ad355ba1dd4440ae036fbafcf8f535198793ef
Josef31 <josef310798@gmail.com>
2026-05-21 16:09:47 -0400

    ejercicios de 5 y 7

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import {ej01} from './ej01.js';
 import {ej03} from './ej03.js';
+import {ej05} from './ej05.js';
+import {ej07} from './ej07.js';
 
 console.log("Ejercicio 1: ");
 ej01();
 console.log('============================');
+
 console.log("Ejercicio 3: ");
 ej03();
 console.log('============================');
+
+console.log("Ejercicio 5: ");
+ej05();
+console.log('============================');
+
+console.log("Ejercicio 7: ");
+ej07();
+console.log('============================');
+
+
 
 console.log("hola mundo");
