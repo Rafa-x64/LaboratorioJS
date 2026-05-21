aee31773a220948987b4b384a7afc3da5bf222ff
Rafa-x64 <alvarezrafaelat@gmail.com>
2026-05-21 15:54:18 -0400

    ejercicios del impares completados

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("hola mundo");
+console.clear();
+
+const ejercicios = ["02", "04", "06", "08", "10", "12", "14", "16", "18", "20"];
+
+for (const num of ejercicios) {
+    console.log(`\n\x1b[1m\x1b[36m--- Ejercicio ${num} ---\x1b[0m`);
+    await import(`./ej${num}.js`);
+}
