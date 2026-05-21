1a2ae05059b2ae339ff98a07f2383c6d1608e15f
Josef31 <josef310798@gmail.com>
2026-05-21 16:00:32 -0400

    ejercicios del 1 al 3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import {ej01} from './ej01.js';
+import {ej03} from './ej03.js';
+
+console.log("Ejercicio 1: ");
+ej01();
+console.log('============================');
+console.log("Ejercicio 3: ");
+ej03();
+console.log('============================');
+
 console.log("hola mundo");
